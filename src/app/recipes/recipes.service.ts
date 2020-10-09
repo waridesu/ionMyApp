f87b7e83942d1f9c3a949d0addd53803cdf1d9fc
Denis <waridesu@gmail.com>
2020-10-09 22:54:23 +0300

    littleLogic

---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {Recipe} from './recipe.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RecipesService {
+    private recipes: Recipe [] = [
+        {
+            id: 'r1',
+            title: 'Schnitzel',
+            imageUrl: 'https://picsum.photos/200/300',
+            ingredients: ['French Fries', 'Pork Meat', 'Salad']
+        },
+        {
+            id: 'r2',
+            title: 'Don\'t know',
+            imageUrl: 'https://picsum.photos/250/300',
+            ingredients: ['Spaghetti', 'Meat', 'Tomatoes']
+        },
+        {
+            id: 'r3',
+            title: 'Bagget Sandwich',
+            imageUrl: 'https://picsum.photos/300/300',
+            ingredients: ['Beget Sandwich', 'Pork Meat', 'Salad']
+        },
+    ];
+
+    constructor() {
+    }
+
+    getAllRecipes() {
+        return [...this.recipes];
+    }
+
+    getRecipe(recipeId: string) {
+        return {
+            ...this.recipes.find(recipe => {
+                return recipe.id === recipeId;
+            })
+        };
+    }
+    deleteRecipe(recipeId: string){
+        this.recipes = this.recipes.filter(recipe => {
+            return recipe.id !== recipeId;
+        });
+    }
+}
